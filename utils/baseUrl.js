75c7f09308c586adb45d4602e5ca1e4eb1b16141
Musharrafbegim <mukhtorova_b@mail.ru>
2023-04-26 13:31:50 +0500

    basurl

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
 	process.env.NODE_ENV === "production"
-		? "https://vercel-musharrafbegim.vercel.app/"
+		? "https://vercel-jiq5vh19i-musharrafbegim.vercel.app"
 		: "http://localhost:3000";
 
 export default baseUrl;
